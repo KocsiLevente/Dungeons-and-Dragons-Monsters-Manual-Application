668eee725caede43ff140d90579633b7ebcfe0f7
Kocsi Levente <kocsilevi@gmail.com>
2023-05-03 18:37:56 +0200

    Extending InjectorUtils with retrofit object creation. Extending the Dao and Repository classes with the new calls. Wire in dependencies.

--- app/src/main/java/com/example/dungeonsanddragonsmonstersmanualapplication/data/MonsterRepository.kt
+++ app/src/main/java/com/example/dungeonsanddragonsmonstersmanualapplication/data/MonsterRepository.kt
@@ -1,18 +1,99 @@
 package com.example.dungeonsanddragonsmonstersmanualapplication.data
 
+import android.util.Log
+import androidx.lifecycle.LiveData
 import com.example.dungeonsanddragonsmonstersmanualapplication.models.Monster
+import com.example.dungeonsanddragonsmonstersmanualapplication.models.MonsterDetailsResult
+import com.example.dungeonsanddragonsmonstersmanualapplication.models.MonsterElement
+import com.example.dungeonsanddragonsmonstersmanualapplication.models.MonstersResult
+import com.example.dungeonsanddragonsmonstersmanualapplication.utils.InjectorUtils
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MonsterRepository private constructor(private val monsterDao: MonsterDao) {
 
-    fun addMonster(monster: Monster) {
+    fun addMonster(monster: MonsterElement) {
         monsterDao.addMonster(monster)
     }
 
-    fun getMonster(id: Int) {
-        monsterDao.getMonster(id)
+    fun getMonster(index: String): MonsterElement {
+        return monsterDao.getMonster(index)
     }
 
-    fun getMonsters() = monsterDao.getMonsters()
+    fun getMonsterDetails(index: String): LiveData<Monster> {
+        val retrofit = InjectorUtils.getRetrofitObject()
+        val dndApi = InjectorUtils.createDnDApiInterface(retrofit)
+        val call = dndApi.getMonsterDetails(index)
+
+        call.enqueue(object : Callback<MonsterDetailsResult> {
+            override fun onFailure(call: Call<MonsterDetailsResult>?, t: Throwable?) {
+                Log.v("FAILURE: Retrofit", " getMonsterDetails call failed!")
+            }
+
+            override fun onResponse(call: Call<MonsterDetailsResult>?, response: Response<MonsterDetailsResult>?) {
+                if (!response!!.isSuccessful) {
+                    Log.v("FAILURE: Retrofit", " getMonsterDetails call response " + response.code() + "!")
+                }
+                else {
+                    val monsterDetails = response.body()
+                    if (monsterDetails != null && monsterDetails.armor_class.isNotEmpty()) {
+                        val monsterToCreate = Monster(1, monsterDetails.index, monsterDetails.image, monsterDetails.name, monsterDetails.desc, monsterDetails.size, monsterDetails.type, monsterDetails.subtype,
+                            monsterDetails.alignment, monsterDetails.armor_class[0].value, monsterDetails.armor_class[0].type, monsterDetails.hit_points, monsterDetails.hit_points_roll,
+                            monsterDetails.speed.toString(), monsterDetails.strength, monsterDetails.dexterity, monsterDetails.constitution, monsterDetails.intelligence, monsterDetails.wisdom,
+                            monsterDetails.charisma, monsterDetails.xp, monsterDetails.damage_vulnerabilities, monsterDetails.damage_resistances, monsterDetails.damage_immunities,
+                            monsterDetails.languages, monsterDetails.actions.map { a -> a.name + ": " + a.desc })
+                        monsterDao.setMonsterDetails(monsterToCreate)
+                    }
+                }
+            }
+        })
+
+        return monsterDao.getMonsterDetails()
+    }
+
+    fun setMonsterDetails(monster: Monster) {
+        monsterDao.setMonsterDetails(monster)
+    }
+
+    fun getMonsters(): LiveData<List<MonsterElement>> {
+        val retrofit = InjectorUtils.getRetrofitObject()
+        val dndApi = InjectorUtils.createDnDApiInterface(retrofit)
+        val call = dndApi.getMonsters()
+
+        call.enqueue(object : Callback<MonstersResult> {
+            override fun onFailure(call: Call<MonstersResult>?, t: Throwable?) {
+                Log.v("FAILURE: Retrofit", " getMonsters call failed!")
+            }
+
+            override fun onResponse(call: Call<MonstersResult>?, response: Response<MonstersResult>?) {
+                if (!response!!.isSuccessful) {
+                    Log.v("FAILURE: Retrofit", " getMonsters call response " + response.code() + "!")
+                }
+                else {
+                    val monsters = response.body()
+                    monsterDao.deleteMonsters()
+                    for (m: MonsterElement in monsters!!.results) {
+                        monsterDao.addMonster(m)
+                    }
+                }
+            }
+        })
+
+        return monsterDao.getMonsters()
+    }
+
+    fun updateMonster(monsterToUpdate: MonsterElement) {
+        monsterDao.updateMonster(monsterToUpdate)
+    }
+
+    fun deleteMonster(index: String) {
+        monsterDao.deleteMonster(index)
+    }
+
+    fun deleteMonsters() {
+        monsterDao.deleteMonsters()
+    }
 
     companion object {
         @Volatile private var instance: MonsterRepository? = null
--- app/src/main/java/com/example/dungeonsanddragonsmonstersmanualapplication/viewmodels/MonsterListViewModel.kt
+++ app/src/main/java/com/example/dungeonsanddragonsmonstersmanualapplication/viewmodels/MonsterListViewModel.kt
@@ -1,9 +1,12 @@
 package com.example.dungeonsanddragonsmonstersmanualapplication.viewmodels
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import com.example.dungeonsanddragonsmonstersmanualapplication.data.MonsterRepository
+import com.example.dungeonsanddragonsmonstersmanualapplication.models.MonsterElement
 
 class MonsterListViewModel(private val monsterRepository: MonsterRepository) : ViewModel() {
 
-    fun getMonsters() = monsterRepository.getMonsters()
+    val monsters: LiveData<List<MonsterElement>>
+        get() = monsterRepository.getMonsters()
 }
